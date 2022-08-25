273879900620273df166e741b6b73d97645a4a18
KaaPlya <100419155+KaaPlya@users.noreply.github.com>
2022-08-25 20:39:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 
-API_ID = os.getenv('API_ID',17269956)                             
-API_HASH = os.getenv('API_HASH','abc7dbe91ecc9f9d157cedc0ce53f2d0')               
-BOT_TOKEN = os.getenv('BOT_TOKEN','5606637267:AAHH6vqUjzF2z92cI6qIoAHgOi7usHLVgK8')           
-BOT_NAME = os.getenv('BOT_NAME','Search Music')           
-GENIUS_API = os.getenv('GENIUS_API','SAlxMepThQDEhCysRfB2J6BXHVDBSkyO9ELJE4QSZ4IFgwDWtrUM1m-SEO-7hn9d')        
+API_ID = os.getenv('API_ID',your api)                             
+API_HASH = os.getenv('API_HASH','your API hash')               
+BOT_TOKEN = os.getenv('BOT_TOKEN','your bot token')           
+BOT_NAME = os.getenv('BOT_NAME','your bot name')           
+GENIUS_API = os.getenv('GENIUS_API','your genius API')        
